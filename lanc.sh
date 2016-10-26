9f793b025bea0dcbe05cf098048755086bdf1267
Nelson Gill <RobotRogue@users.noreply.github.com>
2016-10-26 12:40:49 -0700

    Helper File 1

---
+++ lanc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo " "; echo "-- Navigating to Lanc project --"
+cd $HOME/git/lan-progressive
+pwd
+
+echo " "; echo "-- Performing a Git Pull --"
+git pull
+echo " " ; echo "- Done."
+
+echo " "; echo "-- Performing NPM Install --"
+npm i
+echo " "; echo "- Done."
+
+echo " "; echo "-- Latest Commit Hash --"
+git --no-pager log -n 1
+
+echo " "; echo "-- Running Latest Build --"
+npm run dev
