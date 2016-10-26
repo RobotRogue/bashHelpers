c6d28ae437179585f20822982255c408c39e540d
Nelson Gill <RobotRogue@users.noreply.github.com>
2016-10-26 12:46:26 -0700

    Create a new Bug in Jira bookmatklet
    Create a bookmark, then edit it, then paste this into the URL field.

---
+++ newBugBookmarklet.js
@@ -0,0 +1,3 @@
+javascript: (function () {   
+    var t = document.getElementById("description");   
+    if (t) {t.value ="*URL Tested:*\n- \n \n*Description:*\n- \n \n*Steps to Reproduce:*\n# Navigate to (URL)\n# \n \n*Tested Behavior:*\n- \n \n*Expected Behavior:*\n- \n \n *Notes:*\n-";}})();
