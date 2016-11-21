23706296a2a9cc305df0fafa0f9631ae9e1aa4a9
Nelson Gill <RobotRogue@users.noreply.github.com>
2016-11-21 09:06:01 -0800

    update spacing

--- newJiraBugVerificationComment.js
+++ newJiraBugVerificationComment.js
@@ -1,3 +1,3 @@
 javascript:(function () {
-    var t = document.querySelector('#comment');
-    if (t) {t.value ="*Verified on*: [develop - ABCD1234] - against https://\n\n*Devices Checked*:\n- Chrome Device Sim (Galaxy S5 equivalent)\n- iPhone 6S, iOS 10\n- LG Nexus 5X, 7.0.1\n\n*Notes*:\n- Ticket implemented successfully. Tested vs Expected behavior match. Passed testing.\n\n";}})();
+  var t = document.querySelector('#comment');
+  if (t) {t.value ="*Verified on*: [develop - ABCD1234] - against https://\n\n*Devices Checked*:\n- Chrome Device Sim (Galaxy S5 equivalent)\n- iPhone 6S, iOS 10\n- LG Nexus 5X, 7.0.1\n\n*Notes*:\n- Ticket implemented successfully. Tested vs Expected behavior match. Passed testing.\n\n";}})();
