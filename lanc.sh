82fd8e993a27762880976c286ec25c825e3cf2d8
Nelson Gill <RobotRogue@users.noreply.github.com>
2016-12-09 10:02:13 -0800

    Dump some output to /dev/null

--- lanc.sh
+++ lanc.sh
@@ -5,17 +5,17 @@ cd $HOME/git/lancome-progressive
 PWD
 
 echo " "; echo "-- Performing a Git Pull --"
-git pull
+git pull &> /dev/null
 echo " " ; echo "- Done."
 
 echo " "; echo "-- Performing NPM Install --"
-npm i
+npm i &> /dev/null
 echo " "; echo "- Done."
 
 echo " "; echo "-- Current Branch and Commit --"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 COMMIT=$(git rev-parse --short HEAD)
-echo "$BRANCH - $COMMIT"
+echo " "; echo "$BRANCH - $COMMIT"
 
 echo " "; echo "-- Running Latest Build --"
 npm run dev
