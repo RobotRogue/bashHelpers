a5d261a11fc98f8da7b35f11bca13d77b7641905
Nelson Gill <RobotRogue@users.noreply.github.com>
2017-01-26 13:24:52 -0800

    Add Branch+Commit SHA line
    It's becoming more important to include tested branch/commit - especially for projects with multiple branches in active dev.

--- newBugBookmarklet.js
+++ newBugBookmarklet.js
@@ -1,3 +1,3 @@
 javascript: (function () {   
     var t = document.getElementById("description");   
-    if (t) {t.value ="*URL Tested:*\n- \n \n*Description:*\n- \n \n*Steps to Reproduce:*\n# Navigate to (URL)\n# \n \n*Tested Behavior:*\n- \n \n*Expected Behavior:*\n- \n \n *Notes:*\n-";}})();
+    if (t) {t.value ="*URL Tested:*\n- \n \n*Description:*\n- \n \n*Steps to Reproduce:*\n# Navigate to (URL)\n# \n \n*Tested Behavior:*\n- \n \n*Expected Behavior:*\n- \n \n *Notes:*\n- Branch + Commit SHA:\n- ";}})();
