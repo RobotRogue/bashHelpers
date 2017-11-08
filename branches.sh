fabfb822dd97bc90bccbea1fdf7075ea6a5884aa
Nelson Gill <RobotRogue@users.noreply.github.com>
2017-11-08 10:19:01 -0800

    Modify verbiage

--- branches.sh
+++ branches.sh
@@ -9,5 +9,5 @@ cd $HOME/git/(your frontend project)
 FEBRANCH=$(git rev-parse --abbrev-ref HEAD)
 FECOMMIT=$(git rev-parse --short=8 HEAD)
 
-echo " "; echo "BE - [$BEBRANCH - $BECOMMIT]"
-echo "FE - [$FEBRANCH - $FECOMMIT]"
+echo " "; echo "Backend:  [$BEBRANCH - $BECOMMIT]"
+echo "Frontend: [$FEBRANCH - $FECOMMIT]"
