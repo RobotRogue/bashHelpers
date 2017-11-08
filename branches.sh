8903aaf23cfbbd775f0bbb4f3ea3dc088d03eb3a
Nelson Gill <RobotRogue@users.noreply.github.com>
2017-11-08 10:13:48 -0800

    Added branch and commit script
    - Add script
    - Need to put script in /usr/local/bin
    - Need to run chmod a+x (filename.sh)
    - Set alias (ie: alias branches="/usr/local/bin/branches.sh")

---
+++ branches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+
+cd $HOME/git/(your backend project)
+BEBRANCH=$(git rev-parse --abbrev-ref HEAD)
+BECOMMIT=$(git rev-parse --short=8 HEAD)
+
+cd $HOME/git/(your frontend project)
+FEBRANCH=$(git rev-parse --abbrev-ref HEAD)
+FECOMMIT=$(git rev-parse --short=8 HEAD)
+
+echo " "; echo "BE - [$BEBRANCH - $BECOMMIT]"
+echo "FE - [$FEBRANCH - $FECOMMIT]"
