a90338f930ab1a802eda48503a12c94c1ab9f666
Nelson Gill <RobotRogue@users.noreply.github.com>
2018-05-11 11:21:14 -0700

    Update to include new user name

--- insertToTextArea.js
+++ insertToTextArea.js
@@ -1,5 +1,6 @@
 // Bookmarklet for adding text into a `textarea` tag in a page (handy for greeting messages in the Discord web view)
 
 javascript: (function () {
-    document.getElementsByTagName('textarea')[0].value = 'Welcome @USER ! Please read the #rules and set your location in #role-request if applicable.';
+    var x = document.querySelector('a[rel="noreferrer"]').textContent;
+    document.getElementsByTagName('textarea')[0].value = 'Welcome @' + x + ' ! Please read the #rules and set your location in #role-request if applicable.';
 })();
