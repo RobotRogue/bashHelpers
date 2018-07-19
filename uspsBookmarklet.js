2ec35287fb3ce2e824b2f5e4f2a8e191009ef8f0
Nelson Gill <RobotRogue@users.noreply.github.com>
2018-07-19 12:01:53 -0700

    Add bookmarklet

---
+++ uspsBookmarklet.js
@@ -0,0 +1,1 @@
+javascript: (function () { var tracking = "https://tools.usps.com/go/TrackConfirmAction?tLabels=" + prompt("Paste your USPS Tracking here"); console.log(tracking); window.open(tracking) })();
